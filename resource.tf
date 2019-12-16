8caffe56632f39d772d48bc41e5a11049c2fabfe
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-12-17 01:19:14 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -4,6 +4,12 @@ variable "location" {
   default = "eu-de-1"
 }
 
+variable "image" {
+  type        = "string"
+  description = "Ex) 7100-05-04, 7200-03-03, IBMi-72-09-002, IBMi-73-06-001, IBMi-74-00-001, ..."
+  default = "IBMi-74-00-001"
+}
+
 data "ibm_resource_group" "group" {
   name = "khayama-rg"
 }
@@ -16,3 +22,19 @@ resource "ibm_resource_instance" "resource_instance" {
   resource_group_id = "${data.ibm_resource_group.group.id}"
   tags              = ["user:khayama"]
 }
+
+resource "ibm_pi_instance" "test-instance" {
+    pi_instance_name      = "test-vm"
+    pi_replicants         = "1"
+    pi_key_pair_name      = "khayama-key"
+    pi_proc_type          = "shared"
+    pi_sys_type           = "s922"  
+    pi_processors         = "0.25"
+    pi_memory             = "2"
+    pi_image_id           = "${var.image}>"
+    pi_volume_ids         = []
+    pi_network_ids        = ["<id of the VM's network IDs>"]
+    pi_migratable         = "true"
+    pi_replication_policy = "none"
+    pi_cloud_instance_id  = "${data.ibm_resource_instance.resource_instance.id}"
+  }
