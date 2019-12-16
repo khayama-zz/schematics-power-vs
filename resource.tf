ce2898d556acc7771e3d8445814a085520b252ba
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-12-17 01:00:18 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,3 +1,9 @@
+variable "location" {
+  type        = "string"
+  description = "Ex) us-south, us-east, eu-de-1, ..."
+  default = "eu-de-1"
+}
+
 data "ibm_resource_group" "group" {
   name = "khayama-rg"
 }
@@ -6,7 +12,7 @@ resource "ibm_resource_instance" "resource_instance" {
   name              = "khayama-power"
   service           = "power-iaas"
   plan              = "power-virtual-server-group"
-  location          = "eu-de-1"
+  location          = "${var.location}"
   resource_group_id = "${data.ibm_resource_group.group.id}"
   tags              = ["user:khayama"]
 }
