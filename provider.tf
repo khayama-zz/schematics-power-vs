0577d093329b364fdaf2610ffe4f5bf9592e99b2
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-12-17 10:42:37 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 variable "ibmcloud_api_key" {
-  description = "Enter the API key to access IBM Cloud classic infrastructure. For more information for how to create an API key and retrieve it, see [Managing classic infrastructure API keys](https://cloud.ibm.com/docs/iam?topic=iam-classic_keys)."
+  description = "Enter the API key to access IBM Cloud."
 }
 
 provider "ibm" {
